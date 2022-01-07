293fe5d3596f8effa163aecab595bd318f945784
Ivan Korolkov <korolkov.ivan@mail.ru>
2022-01-07 13:01:25 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+  let res = 0;
+    while(n != 0)
+    {
+        res = (res * 10) + n % 10;
+        n = Math.trunc(n / 10);
+    }
+    if(res < 0){
+      res = -res;
+    }
+    return res;
+
 }
